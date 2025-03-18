273522c16d51218283b37f534a7822e98a9648a7
rajat <rajat290@users.noreply.github.com>
2025-03-18 23:58:22 +0530

    task discription

--- APIPRACTCIALS/About tasks/01.js
+++ APIPRACTCIALS/About tasks/01.js
@@ -1,1 +1,3 @@
 // Fetch and Display Data from a Public API
+// 📌 Task Description:
+// You need to fetch user data from the JSONPlaceholder API and display user names in a list. The API provides dummy user data, and your task is to retrieve and display it in a React component.
