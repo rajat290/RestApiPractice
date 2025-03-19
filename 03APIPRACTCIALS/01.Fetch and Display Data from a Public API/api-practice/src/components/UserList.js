50a7d795e690b3e3def21b93fe1baf702ab4c7c9
rajat <rajat290@users.noreply.github.com>
2025-03-19 20:15:01 +0530

    fix the errors

---
+++ 03APIPRACTCIALS/01.Fetch and Display Data from a Public API/api-practice/src/components/UserList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+const Userlist = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setloading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("failed to fetch users");
+
+                }
+                return response.json();
+            })
+            .then((data) => {
+        setUsers(data); //store api in state 
+                setUsers(data); //store api in state 
+                setloading(false);
+
+            });
+
+    }, []);
+
+    if (loading) return <p>Loading Users....</p>
+    if (error) return <p> Error: {error.message} </p>;
+
+    return (
+
+        <div>
+            <h1> Userlist </h1>
+            <ul> {users.map((user) => (
+                <li key={user.name}>{user.name}</li> // display user names
+            ))}</ul>
+        </div>
+
+
+    )
+
+
+}
+
+export default Userlist;
