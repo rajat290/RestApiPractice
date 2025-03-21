2560b292d8eac1c5781bcd943e7be621e04fc7bc
rajat <rajat290@users.noreply.github.com>
2025-03-22 01:43:45 +0530

    footer added by itself

--- 03APIPRACTCIALS/01.Fetch and Display Data from a Public API/api-practice/src/App.js
+++ 03APIPRACTCIALS/01.Fetch and Display Data from a Public API/api-practice/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 import Userlist from './components/UserList';
+import Footer from './components/Footer';
 // import UsersList from './components/UsersList';
 
 function App() {
   return (
-   <div> 
-    <h1> API Data Fetching</h1>
-    <Userlist />
-    {/* <UsersList /> */}
+    <div>
+      <h1> API Data Fetching</h1>
+      <Userlist />
+      <Footer />
+      {/* <UsersList /> */}
 
-   </div>
+    </div>
   );
 }
 
---
+++ 03APIPRACTCIALS/01.Fetch and Display Data from a Public API/api-practice/src/components/Footer.js
@@ -0,0 +1,23 @@
+// Filename - components/Footer.js
+
+import React, { useLayoutEffect } from "react";
+// import {
+//     Box,
+//     FooterContainer,
+//     Row,
+//     Column,
+//     FooterLink,
+//     Heading,
+// } from "./FooterStyles";
+
+const Footer = () => {
+    return (
+       <footer>
+        <ul>
+            <li><a href="#">Home</a></li>
+            
+        </ul>
+       </footer>
+    );
+};
+export default Footer;
